828cf162ce4cc1bbc7cd3a32652bd55e688ad8a4
pavvo <vv.pavlee@gmail.com>
2019-03-25 23:00:25 +0100

    Connected react to apollo

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components'
 
 const ALL_ITEMS_QUERY = gql`
     query ALL_ITEMS_QUERY {
@@ -15,19 +16,38 @@ const ALL_ITEMS_QUERY = gql`
     }
 `;
 
+const Center = styled.div`
+    text-align: center
+`
+
+const ItemsList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${ props => props.theme.maxWidth};
+    margin: 0 auto;
+`
+
 class Items extends Component {
     render() {
         return (
             <div>
-               <p>Items!</p>
-               <Query query={ALL_ITEMS_QUERY}>
-                    {
-                        ({data, error, loading}) => {
-                            console.log(data)
-                            return <p>Something</p>
-                        }
-                    }
-               </Query>
+                <Center>
+                    <p>Items!</p>
+                    <Query query={ALL_ITEMS_QUERY}>
+                            {
+                                ({data, error, loading}) => {
+                                    if(error) {
+                                        return <p>Error: {error.message}</p>
+                                    } 
+                                    if(loading) {
+                                        return <p>Loading...</p>
+                                    }
+                                    return <ItemsList>{data.items.map( item => { return <p>{item.title}</p> })} </ItemsList>
+                                }
+                            }
+                    </Query>
+               </Center>
             </div>
         );
     }
